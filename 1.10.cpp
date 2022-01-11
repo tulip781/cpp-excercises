c014cee98704630c57c90fd4e04225c74e82c16f
Oliver Inglis <ollie@theinglistwins.com>
2022-01-11 17:52:21 +0000

    more excercises

---
+++ 1.10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Printing Number from 10 to 0" << endl;
+    int i = 10;
+    while (i>=0){
+        cout << i << endl;
+        --i;
+    }
+    return 0;
+}
