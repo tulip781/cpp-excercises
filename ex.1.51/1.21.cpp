843a3ed2683d596ca69e3c9b6f9eb291fee03e55
Oliver Inglis <ollie@theinglistwins.com>
2022-01-18 18:16:10 +0000

    New excercise for 1.21

---
+++ ex.1.51/1.21.cpp
@@ -0,0 +1,12 @@
+#include <iostream>;
+#include "Sales_item.h";
+using namespace std;
+
+int main (){
+    Sales_item item1, item2; 
+    Sales_item item1, item2;
+    std::cin >> item1 >> item2;
+    std::cout << item1 + item2 << std::endl;
+
+    return 0
+}
